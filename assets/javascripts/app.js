5fe1003b341b71f988e24dec45a84a2f2b344693
Ben Hull <ben@spookandpuff.com>
2014-07-10 00:31:15 +0930

    Styling and adding modernizr detect for touch

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -1,7 +1,12 @@
 //= require_tree ./vendor
 
 $(function() {
+  if (!Modernizr.touch) {
     var BV = new $.BigVideo();
     BV.init({useFlashForFirefox:false, container:$('#home.section')});
+    BV.getPlayer().on('loadeddata', function(){
+      $('html').addClass('video-loaded');
+    })
     BV.show('http://winter-feast.s3.amazonaws.com/video/fire-slow.mp4', {ambient:true, container: $('#home.section')});
+  }
 });
